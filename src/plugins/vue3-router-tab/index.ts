3ea13c1ece53808b1f1805b55bb8a4361631f92d
newJcole <stussury@gmail.com>
2023-04-05 00:24:33 +0800

    add lint

--- src/plugins/vue3-router-tab/index.ts
+++ src/plugins/vue3-router-tab/index.ts
@@ -10,17 +10,30 @@ interface Options {
   router: Router;
 }
 
+/**
+ * get tab id
+ * @param {TabKey} tabKey 
+ * @param {RouteLocationNormalized} router 
+ * @returns {string} 
+ */
 const getTabId = (tabKey: TabKey, router: RouteLocationNormalized) => {
   if (isFunction(tabKey)) return tabKey(router);
   return router[tabKey];
 };
 
+/**
+ * router meta to tab
+ * @param {RouteLocationNormalized} router 
+ * @returns {Tab}
+ */
 const routerMetaToTab = (router: RouteLocationNormalized) => {
   const meta = Object.assign(INITIAL_META, router.meta);
-  const tab={
+  const tab = {
     name: meta.tabName ?? router.name,
     id: getTabId(meta.tabKey, router),
-  }};
+  }
+  return tab;
+};
 
 const interceptRoute = (guard: RouteLocationNormalized) => {
   const tab = routerMetaToTab(guard);
