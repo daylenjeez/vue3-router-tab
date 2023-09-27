e3af68fece2278e53e762cc83958d1ef0671a954
Daylen <stussury@gmail.com>
2023-09-27 10:54:50 +0800

    add tetst

--- src/plugins/vue3-router-tab/tests/actions.test.ts
+++ src/plugins/vue3-router-tab/tests/actions.test.ts
@@ -1,14 +1,8 @@
-// index.js
-
 import { createRouter, createMemoryHistory } from 'vue-router';
-import {describe, expect, it}from 'vitest';
-import { createApp, ref } from 'vue';
-import routerTab from "..";
-import { useRouterTab } from '..';
+import {describe, it}from 'vitest';
+import { createApp } from 'vue';
+import routerTab,{ useRouterTab } from "..";
 import App from "../../../App.vue";
-
-// 假设的tabs数组
-const tabs = ref([]);
 
 // 创建一个内存路由
 const history = createMemoryHistory();
@@ -16,16 +10,9 @@ const router = createRouter({
   history,
   routes: [
     { path: '/initial', component: {},name:'initial', },
-    { path: '/path', component: {} },
-    { path: '/fullpath', component: {} }
+    {path: '/path', component: {},meta:{routerTabConfig:{key:'path'}}},
+    { path: '/fullpath', component: {},meta:{routerTabConfig:{key:'fullpath'}} }
   ]
-});
-
-// 监听路由变化，添加新的tab
-router.afterEach((to) => {
-  if (!tabs.value.includes(to.path)) {
-    tabs.value.push(to.path);
-  }
 });
 
 // 单元测试
@@ -35,21 +22,17 @@ describe('Tabs Test', () => {
   app.use(router);
   app.use(routerTab, { router });
 
-  const _routerTab = useRouterTab();
-  it('should add a new tab when router.push is called', async () => {
-    // 初始情况下，tabs应该是空的
-    expect(tabs.value).toEqual([]);
-
-    // 路由跳转到/tab1
+  it('should add a new tab when router.push is called', async ({expect}) => {
+    const _routerTab = useRouterTab();
+    
     await router.push('/initial');
-    console.log(router.currentRoute);
-    
-    console.log(_routerTab.tabs);
-    
-    // expect(tabs.value).toEqual(['/tab1']);
 
-    // 路由跳转到/tab2
-    await router.push('/tab2');
-    expect(tabs.value).toEqual(['/tab1', '/tab2']);
+    expect(_routerTab.tabs[1]).toEqual({
+      "fullPath": "/initial",
+      "id": "/initial",
+      "isIframe": false,
+      "keepAlive": true,
+      "name": "initial",
+    });
   });
 });
