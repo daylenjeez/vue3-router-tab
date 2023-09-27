522e050f5913dc1d1defbb5fdb63524e4ac02d66
Daylen <stussury@gmail.com>
2023-09-27 13:52:32 +0800

    add tetst

--- src/plugins/vue3-router-tab/tests/actions.test.ts
+++ src/plugins/vue3-router-tab/tests/actions.test.ts
@@ -11,12 +11,12 @@ const router = createRouter({
   routes: [
     { path: '/initial', component: {}, name: 'initial', },
     { path: '/path', component: {}, name: 'path', meta: { tabConfig: { key: 'path' } } },
+    { path: '/pathWithParams/:id', component: {}, name: 'pathWithParams', meta: { tabConfig: { key: 'path' } } },
     { path: '/fullpath', component: {}, meta: { tabConfig: { key: 'fullpath' } } }
   ]
 });
 
-// 单元测试
-describe('add tabs', () => {
+describe('Add ta', () => {
   const app = createApp(App);
 
   app.use(router);
@@ -35,16 +35,29 @@ describe('add tabs', () => {
     });
   });
 
-  it(`配置 key:path 时`, async ({ expect }) => {
+  it(`配置 key:path 时，包含 query 的 path，需要去除 query`, async ({ expect }) => {
     const _routerTab = useRouterTab();
 
     await router.push('/path?id=1');
 
-    expect(_routerTab.tabs[2]).toEqual({
+    expect(_routerTab.tabs[_routerTab.tabs.length - 1]).toEqual({
       "fullPath": "/path?id=1",
       "id": "/path",
       "keepAlive": true,
       "name": "path",
+    });
+  });
+
+  it(`配置 key:path 时，包含 params 的 path，不能去除 params`, async ({ expect }) => {
+    const _routerTab = useRouterTab();
+
+    await router.push('/pathWithParams/2');
+
+    expect(_routerTab.tabs[_routerTab.tabs.length - 1]).toEqual({
+      "fullPath": "/pathWithParams/2",
+      "id": "/pathWithParams/2",
+      "keepAlive": true,
+      "name": "pathWithParams",
     });
   });
 });
