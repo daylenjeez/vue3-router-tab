e9eb8020ead84da1026613d793a1d59b0f1924e5
daylenJeez <stussury@gmail.com>
2023-11-01 21:46:07 +0800

    add initial ui

--- src/plugins/vue3-router-tab/components/tabs/tab/close/index.tsx
+++ src/plugins/vue3-router-tab/components/tabs/tab/close/index.tsx
@@ -1,18 +1,8 @@
 import {  PropType, defineComponent,inject } from "vue";
 import { useRouterTabStore } from "@routerTab/store";
 import { TabId, Ui } from "@routerTab/types";
-import ElementClose from "@routerTab/components/elementPlus/icon/close";
+import InitialClose from "@routerTab/components/ui/initial/icon/close";
 import './index.less';
-
-type Close = typeof ElementClose;
-
-const CLOSE_ICON:{[k in Ui]:Close} = {
-  elementPlus: ElementClose,
-  initial:ElementClose,
-  ant:ElementClose,
-  naviUi:ElementClose,
-  tailWind:ElementClose
-};
 
 export default defineComponent({
   name: "RtTabClose",
@@ -24,18 +14,15 @@ export default defineComponent({
   },
   setup(props) {
     const store = useRouterTabStore();
-    const ui = inject<Ui>('ui');
     
     const close = (e: MouseEvent) => {
       store.close({ id: props.id });
       e.stopPropagation();
     };
-
-    const CloseComponent = CLOSE_ICON[ui ?? 'elementPlus'];
     
     return () => (
       <div class="remove-icon" onClick={close}>
-        <CloseComponent />
+        <InitialClose />
       </div>
     );
   },
