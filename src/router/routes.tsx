16fb7dd77726150ba8d6c3251a74fd263ed82622
Daylen <stussury@gmail.com>
2023-11-20 13:12:43 +0800

    modify something

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -16,7 +16,7 @@ const routes: Array<RouteRecordRaw> = [
         meta: { tabConfig: { key: 'path' } }
       },
       {
-        name: "fullpatha",
+        name: "fullpat",
         path: "/fullpath",
         component: () => import("../views/fullPath.vue"),
         meta: { tabConfig: { key: 'fullPath' } }
