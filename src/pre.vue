d781aa8445b55a88ff3f428abad38cac14a086b3
daylenJeez <stussury@gmail.com>
2025-03-03 23:00:17 +0800

    Add Pre component for tab prefix rendering

---
+++ src/pre.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tab-prefix">
+    <span>📌</span>
+  </div>
+</template>
+
+<script lang="ts">
+import type { PropType } from "vue";
+import type { Tab } from "@routerTab/types";
+
+export default {
+  name: "Pre",
+  props: {
+    tab: {
+      type: Object as PropType<Tab>,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-prefix {
+  display: inline-flex;
+  align-items: center;
+  margin-right: 5px;
+  color: var(--tab-color, #666);
+}
+</style>
